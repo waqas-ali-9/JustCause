02680e3cc595918d3247a11a18ad032293762f1d
Waqas Shabbir <wkaxhank@gmail.com>
2023-11-02 22:16:06 +0500

    tada

---
+++ database.js
@@ -0,0 +1,10 @@
+const mysql=require("mysql2");
+console.log("is database ignored or not???")
+console.log("is database ignored or not???")
+module.exports=mysql.createConnection({
+    host:"localhost",
+    port:"3306",
+    user:"root",
+    password:"12345678",
+    database:"db_breed",
+});
